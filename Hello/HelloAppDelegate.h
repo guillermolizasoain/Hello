88614da8211f6bca1ecb1800a0a15beba14df358
Guillermo Lizasoain <guillermolizasoain@Macintosh-8.local>
2012-05-31 16:12:57 -0400

    Initial Commit

---
+++ Hello/HelloAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloAppDelegate.h
+//  Hello
+//
+//  Created by Guillermo Lizasoain on 5/31/12.
+//  Copyright (c) 2012 Blue Bamboo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello/HelloViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloViewController.h
+//  Hello
+//
+//  Created by Guillermo Lizasoain on 5/31/12.
+//  Copyright (c) 2012 Blue Bamboo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloViewController : UIViewController
+
+@end
